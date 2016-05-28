19403bdcf2ba09c0aadb32ce25b0dcb36f0232a3
hyo-jung <knht11@gmail.com>
2016-05-28 17:30:05 +0900

    2016-05-28 계산 프로그램

---
+++ app/src/main/java/com/example/user/mycalc/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.user.mycalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    //변수 설정
+    EditText et_first;
+    EditText et_second;
+    TextView tv_ruslt;
+
+    Button bt_plus;
+    Button bt_subs;
+    Button bt_mult;
+    Button bt_divd;
+    Button bt_mod;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+/*        //속성별 메소드 설정
+        et_first = ;
+        et_second;
+        tv_ruslt;
+        int result = 0;
+
+        bt_plus.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (result = Interger.paseInt(et_first.getText().toString()) + Interger.paseInt(et_second.getText().toString())) {
+                }
+                tv_ruslt.setText(result));
+                return false;
+            }
+        });*/
+
+    }
+}
